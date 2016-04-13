ed9e2540aa4da63fb625226ee8389e042cd68219
Colin Gillespie <csgillespie@gmail.com>
2016-04-13 22:02:31 +0100

    Simpilfy error handling

--- R/check_api_response.R
+++ R/check_api_response.R
@@ -1,12 +1,9 @@
 check_api_response = function(resp, cont) {
   if (httr::status_code(resp) != 200) {
-    parsed = jsonlite::fromJSON(cont, simplifyVector = FALSE)
     stop(
       sprintf(
-        "rtypeform API request failed [%s]\n%s\n<%s>",
-        status_code(resp),
-        parsed$message,
-        parsed$documentation_url
+        "rtypeform API request failed [%s]\n",
+        status_code(resp)
       ),
       call. = FALSE
     )
