35f96316c6689cf1c43c36a42a692f7fa03baffa
Colin Gillespie <csgillespie@gmail.com>
2016-09-11 18:45:57 +0100

    Updatings docs

--- R/get_api.R
+++ R/get_api.R
@@ -1,4 +1,10 @@
-get_api = function(api){
+#' Retrieve API key from Renviron
+#'
+#' If the entry \code{typeform_api} exist in your
+#' \code{.Renviron} return that value. Otherwise, raise an error.
+#' @inheritParams get_results
+#' @export
+get_api = function(api=NULL){
   if(is.null(api))
     api = Sys.getenv("typeform_api")
   if(nchar(api) == 0)
