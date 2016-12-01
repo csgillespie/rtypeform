c4f96c95feaa5df66d2e2c3567669fb18ffb1278
Colin Gillespie <csgillespie@gmail.com>
2016-12-01 21:16:34 +0000

    Prepare for next CRAN

--- tests/testthat/test_get_results.R
+++ tests/testthat/test_get_results.R
@@ -1,6 +1,6 @@
 test_that("Testing get_results_typeforms", {
   skip_on_cran()
-  uid = "o99fIa"
+  uid = "COBOws"
   res = get_results(uid)
   expect_true(is.list(res))
   expect_true(is.list(res$stats))
