237c18ae1e46bf31e2c15d1f3e8087e80509eba8
Colin Gillespie <csgillespie@gmail.com>
2017-05-02 22:10:30 +0100

    Add a print method

---
+++ R/print.R
@@ -0,0 +1,11 @@
+#' @export
+print.rtypeform_results = function(x, ...) {
+  cat("http_status:", x$http_status)
+  cat("\nQuestion statistics:\n")
+  cat("\t Total:", x$stats$total)
+  cat("\n\t Completed:", x$stats$completed)
+  cat("\n\t Uncompleted:", x$stats$uncompleted)
+  cat("\n\t Response rate:", signif(x$stats$completed/x$stats$total, 3)*100, "%", sep="")
+  invisible(x)
+}
+
