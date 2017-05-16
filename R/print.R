921232cf8d472665560aa33f30ada739f992d7cb
Colin Gillespie <csgillespie@gmail.com>
2017-05-16 22:02:53 +0100

    Minor tweaks to the print method

--- R/print.R
+++ R/print.R
@@ -4,8 +4,8 @@ print.rtypeform_results = function(x, ...) {
   cat("\nQuestion statistics:\n")
   cat("\t Total:", x$stats$total)
   cat("\n\t Completed:", x$stats$completed)
-  cat("\n\t Uncompleted:", x$stats$uncompleted)
-  cat("\n\t Response rate:", signif(x$stats$completed/x$stats$total, 3)*100, "%", sep="")
+  cat("\n\t Uncompleted:", x$stats$total - x$stats$completed)
+  cat("\n\t Response rate: ", signif(x$stats$completed/x$stats$total, 3)*100, "%", sep="")
   invisible(x)
 }
 
