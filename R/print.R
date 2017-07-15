6b85453dbab99097e041d5e023ac82eb4ec58ab8
Colin Gillespie <csgillespie@gmail.com>
2017-07-15 19:50:42 +0100

    Add print method for all typeforms

--- R/print.R
+++ R/print.R
@@ -1,6 +1,14 @@
 #' @export
+print.rtypeform_all = function(x, ...) {
+  print(x$content)
+  cat("\nhttp_status:", x$http_status, "OK")
+  cat("\n", nrow(x$content), "questionnaires")
+  invisible(x)
+}
+
+#' @export
 print.rtypeform_results = function(x, ...) {
-  cat("http_status:", x$http_status)
+  cat("http_status:", x$http_status, "OK")
   cat("\nQuestion statistics:\n")
   cat("\t Total:", x$stats$total)
   cat("\n\t Completed:", x$stats$completed)
@@ -8,4 +16,3 @@ print.rtypeform_results = function(x, ...) {
   cat("\n\t Response rate: ", signif(x$stats$completed/x$stats$total, 3)*100, "%", sep="")
   invisible(x)
 }
-
