b87dc50eb58bc0bc681079d90027fbbf22e74b24
Colin Gillespie <csgillespie@gmail.com>
2017-08-22 15:22:07 +0100

    Adding further tests

--- R/check_api_response.R
+++ R/check_api_response.R
@@ -7,7 +7,7 @@ check_api_response = function(resp) {
   } else if(status_code == 403) {
     "403: Expired token, invalid token, or token does not have access permission"
   } else if(status_code == 404) {
-    "404: Type in URL/ Invalid typeform id"
+    "404: Typo in URL/ Invalid typeform id"
   } else {
     sprintf("rtypeform API request failed [%s]\n",
             status_code)
