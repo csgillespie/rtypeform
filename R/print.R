0b6728ed03f3d85a5e98674a49290ed8217d118c
Colin Gillespie <csgillespie@gmail.com>
2017-10-02 21:28:41 +0100

    Add hidden question statistics to print method

--- R/print.R
+++ R/print.R
@@ -9,10 +9,13 @@ print.rtypeform_all = function(x, ...) {
 #' @export
 print.rtypeform_results = function(x, ...) {
   cat("http_status:", x$http_status, "OK")
-  cat("\nQuestion statistics:\n")
-  cat("\t Total:", x$stats$total)
-  cat("\n\t Completed:", x$stats$completed)
-  cat("\n\t Uncompleted:", x$stats$total - x$stats$completed)
-  cat("\n\t Response rate: ", signif(x$stats$completed/x$stats$total, 3)*100, "%", sep="")
+  cat("\nQuestion statistics:")
+  cat("\n\t\t Questions:", sum(!grepl("hidden", x$questions$id)))
+  cat("\n\t\t Hidden:", sum(grepl("hidden", x$questions$id)))
+  cat("\n\t Responses:", x$stats$total)
+  cat("\n\t\t Completed:", x$stats$completed)
+  cat("\n\t\t Uncompleted:", x$stats$total - x$stats$completed)
+  cat("\n\t\t Response rate: ", signif(x$stats$completed/x$stats$total, 3)*100, "%", sep="")
+  cat("\n")
   invisible(x)
 }
