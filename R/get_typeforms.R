6173a87107352ee05e2791565c2b630783a8b39f
Colin Gillespie <csgillespie@gmail.com>
2017-10-05 10:35:40 +0100

    Rename get_all_typeforms to get_typeforms

--- R/get_typeforms.R
+++ R/get_typeforms.R
@@ -7,6 +7,7 @@
 #' the environment variable \code{Sys.getenv("typeform_api")} is used.
 #' @return A list containing content and the response.
 #' @importFrom httr user_agent GET status_code content
+#' @importFrom tibble tibble
 #' @export
 #' @examples
 #' \dontrun{
@@ -17,7 +18,7 @@
 #' ## Use the system variable
 #' get_all_typeforms()
 #' }
-get_all_typeforms = function(api=NULL) {
+get_typeforms = function(api = NULL) {
   api = get_api(api)
   ua = httr::user_agent("https://github.com/csgillespie/rtypeform")
   url = "https://api.typeform.com/v1/forms"
@@ -27,12 +28,20 @@ get_all_typeforms = function(api=NULL) {
   # Sanity check
   check_api_response(resp)
 
+  content = jsonlite::fromJSON(cont)
+  if(NCOL(content) == 0L) contenct = tibble("uid"= "", "name" = "")
+  colnames(content) = c("uid", "name")
+
   # The returned object
   structure(
     list(
-      content = jsonlite::fromJSON(cont),
+      content = content,
       http_status = 200 ## Must be 200 otherwise error
     ),
     class = "rtypeform_all"
   )
 }
+
+#' @export
+#' @rdname get_typeforms
+get_all_typeforms = function(api = NULL) get_typeforms(api)
