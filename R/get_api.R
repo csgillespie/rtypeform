2733711ccfd1b35a4613b53e9c962ba85e6e5c87
Colin Gillespie <csgillespie@gmail.com>
2018-07-26 11:58:41 +0100

    Start of switching to response api #13
    Getting a list of typeforms now works.

--- R/get_api.R
+++ R/get_api.R
@@ -1,13 +1,15 @@
 #' Retrieve API key from Renviron
 #'
-#' If the entry \code{typeform_api} exists in your
+#' If the entry \code{typeform_api_key} exists in your
 #' \code{.Renviron} return that value. Otherwise, raise an error.
 #' @inheritParams get_all_typeforms
 #' @export
-get_api = function(api=NULL){
-  if(is.null(api))
-    api = Sys.getenv("typeform_api")
-  if(nchar(api) == 0)
-    stop("Invalid api key.")
-  api
+get_api = function(api = NULL){
+  if(is.null(api)) api = Sys.getenv("typeform_api_key")
+
+  if(nchar(api) != 0) return(api)
+  if(nchar(Sys.getenv("typeform_api")) > 0)
+    stop("Old key")
+  stop("Invalid api key.")
+
 }
--- R/get_typeforms.R
+++ R/get_typeforms.R
@@ -1,3 +1,30 @@
+send_response = function(api, url) {
+  api = get_api(api)
+  authorization = httr::add_headers(authorization = glue("bearer {api}"))
+  ua = httr::user_agent("https://github.com/csgillespie/rtypeform")
+
+  resp = httr::GET(url,
+                   authorization,
+                   ua)
+
+
+  cont = httr::content(resp, "text", encoding = "UTF-8")
+  content = jsonlite::fromJSON(cont)
+  content
+}
+
+
+
+#' @export
+get_number_of_forms = function(api = NULL,
+                               search = "",
+                               workspace_id = NULL) {
+  url = glue("https://api.typeform.com/forms?page_size={1}&search={search}")
+  content = send_response(api = api, url)
+  content$total_items
+}
+
+
 #' Fetch all available typeforms
 #'
 #' This function returns a two column data frame containing the typeform names and
@@ -6,42 +33,55 @@
 #' @param api Default \code{NULL}. Your private api key. If \code{api} is \code{NULL},
 #' the environment variable \code{Sys.getenv("typeform_api")} is used.
 #' @return A list containing content and the response.
-#' @importFrom httr user_agent GET status_code content
-#' @importFrom tibble tibble
+#' @importFrom httr user_agent GET status_code content add_headers
+#' @importFrom glue glue
+#' @import dplyr
 #' @export
 #' @examples
 #' \dontrun{
-#' api = "XXXXX"
-#' tf = get_all_typeforms(api)
-#' tf$content
-#'
-#' ## Use the system variable
-#' get_all_typeforms()
 #' }
-get_typeforms = function(api = NULL) {
-  api = get_api(api)
-  ua = httr::user_agent("https://github.com/csgillespie/rtypeform")
-  url = "https://api.typeform.com/v1/forms"
-  resp = httr::GET(url, query = list(key = api), ua)
-  cont = httr::content(resp, "text")
+get_forms = function(api = NULL,
+                     page = 1,
+                     page_size = 10,
+                     search = "",
+                     workspace_id = NULL) {
 
-  # Sanity check
-  check_api_response(resp)
+  if (page_size > 200) {
+    warning("Maximum size is 200. Setting page size to 200")
+    page_size = 200
+  }
+  if (!is.null(workspace_id)) {
+    stop("workspace_id's have not yet been implemented.", call. = FALSE)
+  }
 
-  content = jsonlite::fromJSON(cont)
-  if(NCOL(content) == 0L) contenct = tibble("uid"= "", "name" = "")
-  colnames(content) = c("uid", "name")
 
-  # The returned object
-  structure(
-    list(
-      content = content,
-      http_status = 200 ## Must be 200 otherwise error
-    ),
-    class = "rtypeform_all"
-  )
+  url = glue("https://api.typeform.com/forms?page={page}&page_size={page_size}&search={search}")
+  content = send_response(api = api, url)
+
+  ## TODO: Use this to give use message for empty contents
+  #total_items = content$total_items
+  #page_count = content$page_count
+  items = content$items
+  if (length(items) == 0) {
+    items  = tibble(id = "", title = "", last_updated = "", is_public = "",
+                    is_trial = "", questions = "", theme = "", questionnaire_url = "")
+    return(items)
+  }
+
+
+  items = items %>%
+    select(-settings, -self, -theme, -"_links") %>%
+    as_tibble() %>%
+    bind_cols(items$settings,
+              items$self,
+              items$theme,
+              items$`_links`) %>%
+    rename(questions = href,
+           theme = href1,
+           questionnaire_url = display,
+           last_updated = last_updated_at) %>%
+    mutate(last_updated = lubridate::ymd_hms(items$last_updated))
+
+  items
 }
 
-#' @export
-#' @rdname get_typeforms
-get_all_typeforms = function(api = NULL) get_typeforms(api)
