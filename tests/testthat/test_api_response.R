6b6d0bd20ee93d07c368617dc5102e694030ebda
Colin Gillespie <csgillespie@gmail.com>
2022-12-22 17:09:40 +0000

    ci: Add default API key for CI testing

--- tests/testthat/test_api_response.R
+++ tests/testthat/test_api_response.R
@@ -6,7 +6,7 @@ test_that("Testing api response", {
 
 test_that("Testing get_api", {
   skip_on_cran()
-  api2 = Sys.getenv("typeform_api2")
+  api2 = Sys.getenv("typeform_api2", "an_api_test_key")
   expect_equal(get_api(), Sys.getenv("typeform_api2"))
   Sys.unsetenv("typeform_api2")
 
